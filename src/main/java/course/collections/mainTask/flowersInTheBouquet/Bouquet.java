3b750f19bd7867e5b6022ad8f6c280eacfd202fe
Tatiana Lipinskaya <lipinskayatatia@gmail.com>
2021-08-07 21:43:22 +0300

    fixed collections

--- src/main/java/course/collections/mainTask/flowersInTheBouquet/Bouquet.java
+++ src/main/java/course/collections/mainTask/flowersInTheBouquet/Bouquet.java
@@ -2,19 +2,48 @@ package course.collections.mainTask.flowersInTheBouquet;
 
 
 import course.collections.mainTask.flowersInTheBouquet.accessory.Accessory;
+import course.collections.mainTask.flowersInTheBouquet.accessory.AccessoryName;
 import course.collections.mainTask.flowersInTheBouquet.flowers.Flower;
+import course.collections.mainTask.flowersInTheBouquet.flowers.FlowerFreshness;
+import course.collections.mainTask.flowersInTheBouquet.flowers.FlowerName;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Bouquet {
 
-    public static double sumOfBouquet(List<Flower> bouquet, List<Accessory> accessories) {
+    List<Flower> flowerList;
+    List<Accessory> accessoryList;
+
+    public Bouquet(List<Flower> flowerList, List<Accessory> accessoryList) {
+        this.flowerList = flowerList;
+        this.accessoryList = accessoryList;
+    }
+
+    public static List<Flower> createBouquet() {
+        return Arrays.asList(
+                new Flower(FlowerName.ROSE, FlowerFreshness.FRESH, 10.5, 3, 2.5),
+                new Flower(FlowerName.PEONY, FlowerFreshness.FRESH, 15, 3, 1.5),
+                new Flower(FlowerName.GOLDEN_DAISY, FlowerFreshness.CUT_YESTERDAY, 7, 7, 1.0),
+                new Flower(FlowerName.ANEMONE, FlowerFreshness.FRESH, 5, 5, 1.0),
+                new Flower(FlowerName.ROSE, FlowerFreshness.CUT_YESTERDAY, 13, 2, 2.0)
+        );
+    }
+
+    public static List<Accessory> addAccessoryToBouquet() {
+        return Arrays.asList(
+                new Accessory(AccessoryName.PAPER, 1, 4.0),
+                new Accessory(AccessoryName.RIBBON, 2, 0.75)
+        );
+    }
+
+    public static double sumOfBouquet(List<Flower> flowerList, List<Accessory> accessoryList) {
         double summaryOfFlowers = 0;
         double summaryOfAccessories = 0;
-        for (Flower flower : bouquet) {
+        for (Flower flower : flowerList) {
             summaryOfFlowers += flower.getFlowerQuantity() * flower.getFlowerOnePiecePrice();
         }
-        for (Accessory accessory : accessories) {
+        for (Accessory accessory : accessoryList) {
             summaryOfAccessories += accessory.getAccessoryQuantity() * accessory.getAccessoryPriceForOnePiece();
         }
 
@@ -25,9 +54,8 @@ public class Bouquet {
         for (Flower flower : bouquet) {
             if (flower.getFlowerStalkLength() >= fromThisLength &&
                     flower.getFlowerStalkLength() <= toThisLength) {
-                System.out.println("Flower stalk length has next flowers: " + flower);
+                System.out.println(flower);
             }
-
         }
     }
 
--- src/main/java/course/collections/mainTask/flowersInTheBouquet/Runner.java
+++ src/main/java/course/collections/mainTask/flowersInTheBouquet/Runner.java
@@ -1,44 +1,19 @@
 package course.collections.mainTask.flowersInTheBouquet;
 
-import course.collections.mainTask.flowersInTheBouquet.accessory.Accessory;
-import course.collections.mainTask.flowersInTheBouquet.accessory.AccessoryName;
-import course.collections.mainTask.flowersInTheBouquet.flowers.Flower;
 import course.collections.mainTask.flowersInTheBouquet.flowers.FlowerFreshness;
-import course.collections.mainTask.flowersInTheBouquet.flowers.FlowerName;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static course.collections.mainTask.flowersInTheBouquet.Bouquet.*;
 
 public class Runner {
-    public static List<Flower> createBouquet() {
-        return Arrays.asList(
-                new Flower(FlowerName.ROSE, FlowerFreshness.FRESH, 10.5, 3, 2.5),
-                new Flower(FlowerName.PEONY, FlowerFreshness.FRESH, 15, 3, 1.5),
-                new Flower(FlowerName.GOLDEN_DAISY, FlowerFreshness.CUT_YESTERDAY, 7, 7, 1.0),
-                new Flower(FlowerName.ANEMONE, FlowerFreshness.FRESH, 5, 5, 1.0),
-                new Flower(FlowerName.ROSE, FlowerFreshness.CUT_YESTERDAY, 13, 2, 2.0)
-        );
-    }
-
-    public static List<Accessory> addAccessoryToBouquet() {
-        return Arrays.asList(
-                new Accessory(AccessoryName.PAPER, 1, 4.0),
-                new Accessory(AccessoryName.RIBBON, 2, 0.75)
-        );
-    }
-
     public static void main(String[] args) {
-        List<Flower> flowerList = createBouquet();
-        List<Accessory> accessoryList = addAccessoryToBouquet();
+        Bouquet bouquet = new Bouquet(createBouquet(), addAccessoryToBouquet());
 
-
-        System.out.println("Total price of bouquet is: " + sumOfBouquet(flowerList, accessoryList));
+        System.out.println("Bouquet: " + "\n" + bouquet.flowerList + "\n" + bouquet.accessoryList);
+        System.out.println();
+        System.out.println("Total price of bouquet is: " + sumOfBouquet(bouquet.flowerList, bouquet.accessoryList));
         System.out.println("\n" + "Filter flowers with stalk length diapason: ");
-        flowerStalkLengthFilter(flowerList, 4.0, 9.0);
+        flowerStalkLengthFilter(bouquet.flowerList, 4.0, 9.0);
         System.out.println("\n" + "Filter flowers with freshness level: ");
-        flowerFreshFilter(flowerList, String.valueOf(FlowerFreshness.FRESH));
+        flowerFreshFilter(bouquet.flowerList, String.valueOf(FlowerFreshness.FRESH));
     }
-
 }
