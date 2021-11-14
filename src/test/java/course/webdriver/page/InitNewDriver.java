64ab4d4024d4d6338dae93b2b7f51e7a09a55eab
Tatiana Lipinskaya <lipinskayatatia@gmail.com>
2021-11-15 00:22:22 +0300

    added new task

---
+++ src/test/java/course/webdriver/page/InitNewDriver.java
@@ -0,0 +1,31 @@
+package course.webdriver.page;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+public abstract class InitNewDriver {
+    protected WebDriver driver;
+
+    public InitNewDriver(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    public void waitForPageLoad() {
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, 30);
+        wait.until(driver -> {
+            System.out.println("Current Window State       : "
+                    + ((JavascriptExecutor) driver).executeScript("return document.readyState"));
+            return String
+                    .valueOf(((JavascriptExecutor) driver).executeScript("return document.readyState"))
+                    .equals("complete");
+        });
+    }
+
+
+}
